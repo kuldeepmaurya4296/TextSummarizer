3d7efb8f0b7493164c285802643bdc060caac897
kuldeep maurya <kuldeepmaurya4296@gmail.com>
2024-05-05 14:38:17 +0530

    Update app.py
    update app.py

--- app.py
+++ app.py
@@ -3,13 +3,14 @@ from text_summary import summarizer
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/analyze',methods=['GET','POST'])
+
 def analyze():
+    print("Analyze rout")
     if request.method=='POST':
         rawtext = request.form['rawtext']
         summary, original_txt, len_org_txt,len_summary = summarizer(rawtext)
@@ -19,3 +20,4 @@ def analyze():
     
 if __name__ == "__main__":
     app.run(debug=True)
+    
